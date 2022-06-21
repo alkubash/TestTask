4b747c0ae571ea6c271c5d1600f0fc4eca82e486
alkubash <san4ez12@gmail.com>
2022-06-21 17:04:52 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+Console.WriteLine("Введите путь к текстовому файлу:");
+string path = Console.ReadLine();
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+try
+{
+    using (FileStream fstream = System.IO.File.OpenRead(path))
+    {
+        byte[] buffer = new byte[fstream.Length];
+        await fstream.ReadAsync(buffer, 0, buffer.Length);
+        string textFromFile = Encoding.Default.GetString(buffer);
+        Console.WriteLine($"Текст из файла: {textFromFile}");
+
+        var groups = SwimParts(textFromFile, 3)
+                .Where(str => str.All(ch => char.IsLetter(ch)))
+                .GroupBy(str => str);
+
+
+        Console.WriteLine(string.Join
+            (
+                Environment.NewLine,
+                groups.OrderByDescending(gr => gr.Count()).Take(10).Select(gr => $"\"{gr.Key}\" встретилось {gr.Count()} раз")
+            ));
+    }   
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    TimeSpan ts = stopWatch.Elapsed;
+    string elapsedTime = String.Format("{0:00} часов, {1:00} минут, {2:00} секунд, {3:000} миллисекунд",
+                    ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+    Console.WriteLine("Время выполнения программы: " + elapsedTime);
+}
+
+static IEnumerable<string> SwimParts(string source, int length)
+{
+    for (int i = length; i <= source.Length; i++)
+        yield return source.Substring(i - length, length);
+}
+
+
